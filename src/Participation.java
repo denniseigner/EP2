27b40b60a2cba9e2bc59f7c52835a10d7af7af1e
Franz Puntigam <franz@complang.tuwien.ac.at>
2019-03-07 15:09:11 +0100

    Participation revised

--- src/Participation.java
+++ src/Participation.java
@@ -3,29 +3,54 @@ This class will be extended in the ad-hoc-assignment, and will be used
 in future assignments, so it is highly recommended to solve this
 assignment.
 
-This class is part of software for ski racing administration.  The
-class should contain object variables race, racer and bibnumber
-(Startnummer).  Write a constructor for this class that is called
-with, e.g.,
-
-new Participation("Lienz 2011 Ladies' Slalom", "Mikaela Shiffrin", 40)
-
-Add the following non-static methods to this class:
-
-String getRace()
-String getRacer()
-int getBibnumber()
-void print()
-
-print() should output the following (without newline at the start or
-end) for the Participation created by the constructor call above:
-
-40 Mikaela Shiffrin (Lienz 2011 Ladies' Slalom)
-
-Questions:
-
-Should there also be setter methods for the object variables?  Why or
-why not?
+This class is part of software for ski racing administration.
  */
 public class Participation {
+
+    // TODO: The object variables 'race', 'racer' and 'bibnumber' (Startnummer) shall be declared here.
+
+    // TODO: A constructor for this class shall be defined here.
+    // A new object constructed by
+    //
+    //     new Participation("Lienz 2011 Ladies' Slalom", "Mikaela Shiffrin", 40)
+    //
+    // shall specify the name of the race, the racer, and the bibnumber.
+
+
+    // Returns the 'race' of this participation.
+    public String getRace() {
+        // TODO: Implement this method
+        return "";
+    }
+
+    // Returns the 'racer' of this participation.
+    public String getRacer() {
+        // TODO: Implement this method
+        return "";
+    }
+
+    // Returns the 'bibnumber' of this participation.
+    public int getBibnumber() {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    // Output the following (without newline at the start or end) for the Participation created by
+    // the constructor call above:
+    //
+    // 40 Mikaela Shiffrin (Lienz 2011 Ladies' Slalom)
+    //
+    public void print() {
+        // TODO: Implement this method
+    }
+
+    // Question:
+    // Should there also be setter methods for the object variables?  Why or why not?
+
+    // This method is only for testing.
+    // Alternatively, you can put the tests in additional classes.
+    public static void main(String[] args) {
+        // TODO: write your own test cases here if necessary.
+    }
+
 }
