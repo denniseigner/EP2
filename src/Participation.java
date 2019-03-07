7384d295328f7af17a6e9ec8fbefc2442145061d
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2019-03-07 17:52:14 +0100

    Aufgabenblatt 1 revised

--- src/Participation.java
+++ src/Participation.java
@@ -7,12 +7,15 @@ This class is part of software for ski racing administration.
  */
 public class Participation {
 
-    // TODO: The object variables 'race', 'racer' and 'bibnumber' (Startnummer) shall be declared here.
+    // Assignment 1.1:
+    
+    // TODO: The object variables 'race', 'racer' and 'bibnumber'
+    //  (Startnummer) shall be declared here.
 
     // TODO: A constructor for this class shall be defined here.
     // A new object constructed by
     //
-    //     new Participation("Lienz 2011 Ladies' Slalom", "Mikaela Shiffrin", 40)
+    //   new Participation("Lienz 2011 Ladies' Slalom", "Mikaela Shiffrin", 40)
     //
     // shall specify the name of the race, the racer, and the bibnumber.
 
@@ -35,8 +38,8 @@ public class Participation {
         return 0;
     }
 
-    // Output the following (without newline at the start or end) for the Participation created by
-    // the constructor call above:
+    // Output the following (without newline at the start or end) for
+    // the participation created by the constructor call above:
     //
     // 40 Mikaela Shiffrin (Lienz 2011 Ladies' Slalom)
     //
@@ -45,7 +48,9 @@ public class Participation {
     }
 
     // Question:
-    // Should there also be setter methods for the object variables?  Why or why not?
+    
+    // Should there also be setter methods for the object variables?
+    // Why or why not?
 
     // This method is only for testing.
     // Alternatively, you can put the tests in additional classes.
--- src/Startlist.java
+++ src/Startlist.java
@@ -1,53 +1,77 @@
 /*
-This class has an object variable "participations", which is an array
-of Participation objects.
-
-Write a constructor that creates a Startlist with room for 100 entries
-(but none filled at first), with
-
-new Startlist(100)
-
-Write non-static methods
-
-void add(Participation p)
-void print()
-void printOrdered()
-
-where print() prints the filled entries in an arbitrary order, while
-printOrdered() prints them in the order of increasing bib numbers.
-Both print each participant in the same format as produced by
-Participant.print(), followed by a newline.
-
-Questions:
-
-In which sensible ways can the program react if more entries are added
-to the start list than the array has entries?  What does your program
-do?
-
-Frequent requirements for start lists are that all the bib numbers in
-a race are different, contiguous, and start at 1.  We did not require
-these in this assignment.  Did you?  If so, did this simplify the
-implementation?  How much more complex would your program become if
-you lifted one or several of these requirements?  With such
-requirements, what are sensible ways to react to violations of these
-requirements?  In particular, what happens if the same bib number is
-inserted twice?
-
-Another requirement for a start list is that all participations are
-for the same race.  Do you check this?  What is a sensible way to
-react if this is violated?
-
-Assignment 3:
-
-Add the following method to Startlist:
-
-void printpermutations()
-
-prints all the permutations of the startlist, with each permutation
-followed by an empty line.  Use a recursive algorithm.
-
-Questions: How many calls to your recursive method do you get when you
-call printpermutations() on a Startlist with n filled entries?
- */
+This class will be used in future assignments.  It is highly
+recommended to solve Assignment 1.2.
+*/
 public class Startlist {
+
+    // Assignment 1.2:
+    
+    // TODO: The object variable "participations", which is an array
+    //  of Participation objects, shall be declared here.
+
+    // TODO: A constructor for this class shall be defined here.
+    // A new object constructed by
+    //  
+    //   new Startlist(100)
+    //  
+    // has room for 100 Participations (but all NULL at first)
+    
+    // adds Participation p to the Startlist
+    public void add(Participation p) {
+        // TODO: Implement this method
+    }
+
+    // Print the filled entries in an arbitrary order; each
+    // participant is printed in the same format as produced by
+    // Participant.print(), followed by a newline.
+    public void print() {
+        // TODO: Implement this method
+    }
+    
+    // Print the filled entries in the order of increasing bib
+    // numbers; each participant is printed in the same format as
+    // produced by Participant.print(), followed by a newline.
+    public void printOrdered() {
+        // TODO: Implement this method
+    }
+
+    // Questions:
+
+    // 1) In which sensible ways can the program react if more entries
+    // are added to the start list than the array has entries?  What
+    // does your program do?
+    // 
+    // 2) Frequent requirements for start lists are that all the bib
+    // numbers in a race are different, contiguous, and start at 1.
+    // We did not require these in this assignment.  Did you?  If so,
+    // did this simplify the implementation?  How much more complex
+    // would your program become if you lifted one or several of these
+    // requirements?  With such requirements, what are sensible ways
+    // to react to violations of these requirements?  In particular,
+    // what happens if the same bib number is inserted twice?
+    // 
+    // 3) Another requirement for a start list is that all
+    // participations are for the same race.  Do you check this?  What
+    // is a sensible way to react if this is violated?
+
+
+    // Assignment 1.3:
+
+    // Print all the permutations of the startlist, with each
+    // permutation followed by an empty line.
+    public void printpermutations() {
+        // TODO: Implement this method using a recursive algorithm.
+    }
+
+    // Question:
+
+    // How many calls to your recursive method do you get when you
+    // call printpermutations() on a Startlist with n filled entries?
+
+
+    // This method is only for testing.
+    // Alternatively, you can put the tests in additional classes.
+    public static void main(String[] args) {
+        // TODO: write your own test cases here if necessary.
+    }
 }
