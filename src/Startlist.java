df09b78415a5cdf916a55eb8324906a0cdf30ded
Franz Puntigam <franz@complang.tuwien.ac.at>
2019-03-08 12:37:12 +0100

    Revised Aufgabe 1

--- src/Startlist.java
+++ src/Startlist.java
@@ -6,7 +6,7 @@ public class Startlist {
 
     // Assignment 1.2:
     
-    // TODO: The object variable "participations", which is an array
+    // TODO: The object variable 'participations', which is an array
     //  of Participation objects, shall be declared here.
 
     // TODO: A constructor for this class shall be defined here.
@@ -14,23 +14,23 @@ public class Startlist {
     //  
     //   new Startlist(100)
     //  
-    // has room for 100 Participations (but all NULL at first)
+    // has room for 100 participations (all 'null' at first)
     
-    // adds Participation p to the Startlist
+    // adds p to 'this'
     public void add(Participation p) {
         // TODO: Implement this method
     }
 
     // Print the filled entries in an arbitrary order; each
-    // participant is printed in the same format as produced by
-    // Participant.print(), followed by a newline.
+    // participation is printed in the same format as produced by
+    // print() in Participation, followed by a newline.
     public void print() {
         // TODO: Implement this method
     }
     
     // Print the filled entries in the order of increasing bib
-    // numbers; each participant is printed in the same format as
-    // produced by Participant.print(), followed by a newline.
+    // numbers; each participation is printed in the same format as
+    // produced by print() in Participation, followed by a newline.
     public void printOrdered() {
         // TODO: Implement this method
     }
@@ -38,8 +38,8 @@ public class Startlist {
     // Questions:
 
     // 1) In which sensible ways can the program react if more entries
-    // are added to the start list than the array has entries?  What
-    // does your program do?
+    // are added to 'this' than the array has entries?  What does your
+    // program do?
     // 
     // 2) Frequent requirements for start lists are that all the bib
     // numbers in a race are different, contiguous, and start at 1.
@@ -51,22 +51,22 @@ public class Startlist {
     // what happens if the same bib number is inserted twice?
     // 
     // 3) Another requirement for a start list is that all
-    // participations are for the same race.  Do you check this?  What
-    // is a sensible way to react if this is violated?
+    // participations are for the same race.  Do you check this?
+    // What is a sensible way to react if this is violated?
 
 
     // Assignment 1.3:
 
-    // Print all the permutations of the startlist, with each
+    // Print all the permutations of the start list, with each
     // permutation followed by an empty line.
-    public void printpermutations() {
+    public void printPermutations() {
         // TODO: Implement this method using a recursive algorithm.
     }
 
     // Question:
 
     // How many calls to your recursive method do you get when you
-    // call printpermutations() on a Startlist with n filled entries?
+    // call printPermutations() on a start list with n filled entries?
 
 
     // This method is only for testing.
