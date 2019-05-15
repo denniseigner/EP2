0b691f4be742f727261cdae75d6ce9882719f1c5
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2019-05-15 13:58:22 +0200

    AB6

--- src/StringIteratorIterable.java
+++
@@ -1,5 +0,0 @@
-public interface StringIteratorIterable extends java.lang.Iterable<StringIterator> {
-    // creates an iterator for the container 'this'
-    StringIteratorIterator iterator();
-}
-                                
